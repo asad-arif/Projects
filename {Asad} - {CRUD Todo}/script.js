5b508486aad65dbc1d2b6dcccdae6561c92c2418
Asad <asadofficial01@gmail.com>
2023-10-26 12:27:27 +0500

    Todo working

--- {Asad} - {CRUD Todo}/script.js
+++ {Asad} - {CRUD Todo}/script.js
@@ -1,66 +1,135 @@
 const addBtn = document.getElementById('add-btn');
+const updateBtn = document.getElementById('update-btn');
 const inputBox = document.getElementById('input-box');
 const olContent = document.getElementById('content-ol');
+let dbReadValues = [];
 
+//Load data from localstorage on startup
+window.onload = function () {
+  readFromDb();
+  dbReadValues.forEach((value) => {
+    createElement(value);
+  });
+};
+
+//Add the task
 function addTask() {
-  if (inputBox.value !== '') {
+  let check = inputBox.value.trim();
+  if (check !== '') {
     try {
-      const liContainer = document.createElement('div');
-      const btnContainer = document.createElement('div');
-      const completedBtn = document.createElement('button');
-      const deleteBtn = document.createElement('button');
-      const newLi = document.createElement('li');
-      completedBtn.innerText = 'Completed';
-      completedBtn.className = 'completed-btn';
-      deleteBtn.innerText = 'Delete';
-      deleteBtn.className = 'delete-btn';
-      newLi.innerText = inputBox.value;
-
-      btnContainer.appendChild(completedBtn);
-      btnContainer.appendChild(deleteBtn);
-      liContainer.appendChild(newLi);
-      liContainer.append(btnContainer);
-      liContainer.className = 'list-item';
-      olContent.append(liContainer);
-      var btns = document.querySelectorAll('.list-item')[0].children[1];
-
-      // Save the task list to localStorage
-      const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-      tasks.push(inputBox.value);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-
-      completedBtn.addEventListener('click', () => {
-        completed(newLi, liContainer);
-      });
-      deleteBtn.addEventListener('dblclick', () => {
-        deleteContainer(liContainer);
-      });
-      clear();
-      liContainer.addEventListener('mouseover', () => {
-        btnContainer.style.opacity = 1;
-        btnContainer.style.transition = '0.1s ease-in-out';
-      });
-      liContainer.addEventListener('mouseout', () => {
-        btnContainer.style.opacity = 0;
-        btnContainer.style.transition = '0.1s ease-in-out';
-      });
+      writeToDb(inputBox.value);
     } catch (err) {
       console.log('Sorry! Unable to add');
     }
   }
 }
+
+//Delete task from DOM and Local Storage
+function deleteTask(taskDivElement) {
+  taskDivElement.outerHTML = '';
+  const innerLi = taskDivElement.querySelector('.myli');
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    if (localStorage.getItem(key) === innerLi.textContent) {
+      localStorage.removeItem(key);
+    }
+  }
+}
+
+//Modify the task
+function editTask(taskDivElement) {
+  const innerLi = taskDivElement.querySelector('.myli');
+  let oldText = innerLi.textContent;
+  inputBox.value = oldText;
+  addBtn.style.display = 'none';
+  updateBtn.style.display = 'inline';
+
+  updateBtn.addEventListener('click', () => {
+    updateBtn.style.display = 'none';
+    addBtn.style.display = 'inline';
+    let newText = inputBox.value;
+    innerLi.textContent = newText;
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (localStorage.getItem(key) === oldText) {
+        localStorage.setItem(key, newText);
+      }
+    }
+    clear();
+  });
+}
+
+//Write data to Local Storage
+function writeToDb(value) {
+  let key = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  localStorage.setItem(key.toString(), value);
+  createElement(value);
+  readFromDb();
+}
+
+//Read data from Local Storage
+function readFromDb() {
+  let updatedArr = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    updatedArr.push(localStorage.getItem(key));
+    dbReadValues = updatedArr;
+  }
+}
+
+//Create Container and buttons
+function createElement(elem) {
+  const taskDivElement = document.createElement('div');
+  const btnContainer = document.createElement('div');
+  const completedBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
+  const editBtn = document.createElement('button');
+  const newLi = document.createElement('li');
+  newLi.className = 'myli';
+  newLi.textContent = elem;
+  completedBtn.innerText = 'Completed';
+  completedBtn.className = 'completed-btn';
+  editBtn.innerText = 'Edit';
+  editBtn.className = 'edit-btn';
+  deleteBtn.innerText = 'Delete';
+  deleteBtn.className = 'delete-btn';
+  btnContainer.appendChild(completedBtn);
+  btnContainer.appendChild(editBtn);
+  btnContainer.appendChild(deleteBtn);
+  taskDivElement.appendChild(newLi);
+  taskDivElement.append(btnContainer);
+  taskDivElement.className = 'list-item';
+  olContent.append(taskDivElement);
+  completedBtn.addEventListener('click', () => {
+    completed(newLi, taskDivElement);
+  });
+  deleteBtn.addEventListener('click', () => {
+    deleteTask(taskDivElement);
+  });
+  editBtn.addEventListener('click', () => {
+    editTask(taskDivElement);
+  });
+  clear();
+  taskDivElement.addEventListener('mouseover', () => {
+    btnContainer.style.opacity = 1;
+    btnContainer.style.transition = '0.1s ease-in-out';
+  });
+  taskDivElement.addEventListener('mouseout', () => {
+    btnContainer.style.opacity = 0;
+    btnContainer.style.transition = '0.1s ease-in-out';
+  });
+}
+
 function reset() {
   let val = confirm('Are you sure to want reset the Todo-list?');
   if (val) olContent.innerHTML = '';
+  localStorage.clear();
 }
 function clear() {
   inputBox.value = '';
 }
 
-function completed(liElement, liContainer) {
-  liContainer.style.backgroundColor = '#ccf7ff';
+function completed(liElement, taskDivElement) {
+  taskDivElement.style.backgroundColor = '#ccf7ff';
   liElement.style.textDecoration = 'line-through';
-}
-function deleteContainer(liContainer) {
-  liContainer.outerHTML = '';
 }
