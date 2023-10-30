d76acc518a0bf5a83e404238256cbbe47b9ddc11
Asad <asadofficial01@gmail.com>
2023-10-30 14:22:44 +0500

    API Integrated

--- {Asad} - {CRUD Todo}/script.js
+++ {Asad} - {CRUD Todo}/script.js
@@ -2,22 +2,70 @@ const addBtn = document.getElementById('add-btn');
 const updateBtn = document.getElementById('update-btn');
 const inputBox = document.getElementById('input-box');
 const olContent = document.getElementById('content-ol');
+let serverData = [];
 let dbReadValues = [];
 
 // Load data from localstorage on startup
 window.onload = function () {
-  readFromDb();
+  if (localStorage.length === 0) {
+    flag = confirm('Do you want to load server data?');
+    if (flag) {
+      getServerData();
+    }
+  } else {
+    // let flag = false;
+    // flag = confirm('Do you want to load server data?');
+    // if (flag) {
+    //   getServerData();
+    // } else {
+    //   readFromDb();
+    // }
+    readFromDb();
+  }
   dbReadValues.forEach((value) => {
     createElement(value.value, value.styleCheck);
   });
 };
+////////////////////////////////
+//Get and Preprocess server data
+async function getServerData() {
+  let response = await fetch(
+    'https://jsonplaceholder.typicode.com/users/1/todos'
+  );
+  let data = await response.json();
+  serverData = [...data];
+  serverData.forEach((Element) => {
+    writeToDb(Element.title, Element.id, Element.completed);
+  });
+}
+
+//////////////////////////////////
+// send and store data to server()
+function sendToServer() {
+  fetch('https://jsonplaceholder.typicode.com/users/1/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id: 444,
+      title: 'Lorem Ipsum',
+      completed: false,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      writeToDb(data.title, data.id, data.completed);
+    });
+}
+sendToServer();
 
 //Add the task
 function addTask() {
   let taskValue = inputBox.value.trim();
   if (taskValue !== '') {
     try {
-      writeToDb(taskValue);
+      writeToDb(taskValue, null, false);
     } catch (err) {
       console.log('Sorry! Unable to add');
     }
@@ -45,7 +93,7 @@ function editTask(taskDivElement) {
   addBtn.style.display = 'none';
   updateBtn.style.display = 'inline';
 
-  updateBtn.addEventListener('click', () => {
+  function updateTask() {
     updateBtn.style.display = 'none';
     addBtn.style.display = 'inline';
     var newText = inputBox.value;
@@ -60,18 +108,25 @@ function editTask(taskDivElement) {
       }
     }
     clear();
-    window.location.reload();
-  });
+    // Remove the click event listener to avoid multiple listeners
+    updateBtn.removeEventListener('click', updateTask);
+  }
+  updateBtn.addEventListener('click', updateTask);
 }
 
 //Write data to Local Storage
-function writeToDb(taskValue) {
-  let key = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+function writeToDb(taskValue, mykey, completed) {
+  let key = undefined;
+  if (mykey == null) {
+    key = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  } else {
+    key = mykey;
+  }
   localStorage.setItem(
     key.toString(),
     JSON.stringify({
       value: `${taskValue}`,
-      styleCheck: false,
+      styleCheck: completed,
     })
   );
   readFromDb();
